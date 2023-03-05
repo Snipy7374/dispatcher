29c1f4b97ebb7d3cd44b5b95caaa76c9175a8432
Snipy7374 <snipy7374@gmail.com>
2023-03-05 18:04:54 +0100

    add docs to dispatch and remove unused imports

--- ext_dispatcher/dispatcher.py
+++ ext_dispatcher/dispatcher.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
-from typing import Any, TYPE_CHECKING
-from typing_extensions import TypeAlias
+from typing import TYPE_CHECKING
 
-import os
 import logging
 
 if TYPE_CHECKING:
-    from .types import AnyBot
+    from .types import AnyBot  # type: ignore
 
 
 from .generator import Generator
@@ -15,12 +13,38 @@ _log = logging.getLogger(__name__)
 
 
 class Dispatcher:
+    """
+    """
     def __init__(self, bot: AnyBot, library_name: str = "disnake") -> None:
         self.bot = bot
         self.library_name = library_name
         self._generator = Generator(library=library_name)
         self._generator._generate_types()
+    
+    @property
+    def get_listeners(self):
+        self.bot.extra_events
 
     def dispatch(self, event: str, *args, **kwargs) -> None:
+        """
+        Parameters
+        ----------
+        event: :class:`str`
+            The name of the event that should be dispatched.
+        *args
+            Positional arguments that a callback listening for this
+            ``event`` must take.
+        **kwargs
+            Keyword arguments that a callback listening for this ``event``
+            must take.
+
+        Raises
+        ------
+        ValueError
+            ``event`` is not of :class:`str` type.
+        """
+        if not isinstance(event, str):
+            raise ValueError(f"event must be of str type, not {event.__class__!r}")
+
         self.bot.dispatch(event, *args, **kwargs)
         _log.info("%s event was dispatched", event)
--- ext_dispatcher/generator/gen.py
+++ ext_dispatcher/generator/gen.py
@@ -9,7 +9,7 @@ _log = logging.getLogger(__name__)
 class Generator:
     def __init__(self, library: str) -> None:
         self.environment = Environment(
-            loader=FileSystemLoader("ext_dispatcher\\generator\\templates"),
+            loader=FileSystemLoader("ext_dispatcher/generator/templates"),
             lstrip_blocks=True,
             trim_blocks=True,
         )
@@ -23,7 +23,7 @@ class Generator:
     def _generate_types(self):
         """This method handles the logic of file types creation."""
         _log.info("generating file types for %s", self.library_name)
-        if files := os.listdir(path="ext_dispatcher\\types"):
+        if files := os.listdir(path="ext_dispatcher/types"):
             for file in files:
                 # if exists file types of other libraries delete them
                 if not file.startswith(self.library_name):
@@ -36,11 +36,11 @@ class Generator:
                     return
  
         content = self._render_template("types.py.jinja")
-        with open(f"ext_dispatcher\\types\\{self.library_name}_types.py", "w") as file:
+        with open(f"ext_dispatcher/types/{self.library_name}_types.py", "w") as file:
             file.write(content)
         _log.info("created file types for %s at %s", self.library_name, file.name)
 
         init_content = self._render_template("__init__.py.jinja")
-        with open("ext_dispatcher\\types\\__init__.py", "w") as file:
+        with open("ext_dispatcher/types/__init__.py", "w") as file:
             file.write(init_content)
         _log.info("created init file for %s at %s", self.library_name, file.name)
