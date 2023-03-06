d00652ca25e5f749f9df2aa141814a48bec9660f
Snipy7374 <snipy7374@gmail.com>
2023-03-06 17:25:12 +0100

    rename the module to dispatcher and implements the listeners property

--- dispatcher/generator/gen.py
+++ dispatcher/generator/gen.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import shutil
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -23,24 +24,28 @@ class Generator:
     def _generate_types(self):
         """This method handles the logic of file types creation."""
         _log.info("generating file types for %s", self.library_name)
-        if files := os.listdir(path="ext_dispatcher/types"):
+        if files := os.scandir(path="ext_dispatcher/types"):
             for file in files:
                 # if exists file types of other libraries delete them
-                if not file.startswith(self.library_name):
-                    _log.info("found an old file types (%s), removing it", file)
-                    os.remove(f"ext_dispatcher/types/{file}")
-
+                if not file.name.startswith(self.library_name):
+                    if file.is_file():
+                        _log.debug("found an old file types (%s), removing it", file.name)
+                        os.remove(f"ext_dispatcher/types/{file.name}")
+                    else:
+                        shutil.rmtree(f"ext_dispatcher/types/{file.name}")
+                
                 # if file types for the user provided library already exists skip this process
-                if file.startswith(self.library_name):
+                if file.name.startswith(self.library_name):
                     _log.info("%s types already exists, skipping this process", self.library_name)
                     return
  
         content = self._render_template("types.py.jinja")
         with open(f"ext_dispatcher/types/{self.library_name}_types.py", "w") as file:
             file.write(content)
-        _log.info("created file types for %s at %s", self.library_name, file.name)
+        _log.debug("created file types for %s at %s", self.library_name, file.name)
 
         init_content = self._render_template("__init__.py.jinja")
         with open("ext_dispatcher/types/__init__.py", "w") as file:
             file.write(init_content)
-        _log.info("created init file for %s at %s", self.library_name, file.name)
+        _log.debug("created init file for %s at %s", self.library_name, file.name)
+        _log.info("file types created")
